0ac449692cfea13ff4b243ce65f27b31766aba66
emmanueldom103-lang <emmanueldom_103@hotmail.com>
2026-09-19 01:17:31 +0200

    V7.10: renovar cache de la app

--- service-worker.js
+++ service-worker.js
@@ -1,1 +1,5 @@
-const CACHE='radial-opuesto-v7';const ASSETS=['./','./index.html','./styles.css','./app.js','./manifest.webmanifest'];self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
+const CACHE='radial-opuesto-v710';
+const ASSETS=['./','./index.html','./styles.css?v=7.10','./app.js','./manifest.webmanifest'];
+self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)))});
+self.addEventListener('activate',e=>e.waitUntil(Promise.all([caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))),self.clients.claim()])));
+self.addEventListener('fetch',e=>e.respondWith(fetch(e.request).then(r=>{const copy=r.clone();caches.open(CACHE).then(c=>c.put(e.request,copy));return r}).catch(()=>caches.match(e.request))));
